cf252070927848685a223a40e9595bfb7ed1396b
Abhijit RAY <royabhijits928@gmail.com>
2025-10-01 18:43:59 +0530

    Update Commands.sh
    Signed-off-by: Abhijit RAY <royabhijits928@gmail.com>

--- Day-04/Commands.sh
+++ Day-04/Commands.sh
@@ -1,3 +1,6 @@
 sudo dnf install epel-release -y
 sudo dnf install ansible -y
 ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory.ini playbook.yaml
+ansible-playbook -i inventory.ini lbr_setup.yml
+curl -I http://stlb01
+for i in {1..10}; do curl http://stlb01; done
