f0574d64b9dbb561d10f0da9eede0e536dec96d3
Abhijit RAY <royabhijits928@gmail.com>
2025-10-03 01:51:05 +0530

    Linux Process Troubleshooting
    Signed-off-by: Abhijit RAY <royabhijits928@gmail.com>

--- Day-02/Commands.sh
+++ Day-02/Commands.sh
@@ -4,4 +4,4 @@ sudo dnf install epel-release -y
 
 sudo dnf install ansible -y
 
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory.ini playbook.yaml
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory.ini playbook.yaml 
