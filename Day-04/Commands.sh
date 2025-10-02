364498b248b9aadcf3388a189cf0a06eaa1643ce
Abhijit RAY <royabhijits928@gmail.com>
2025-10-03 02:07:50 +0530

    Install and Configure Nginx as an LBR
    Signed-off-by: Abhijit RAY <royabhijits928@gmail.com>

--- Day-04/Commands.sh
+++ Day-04/Commands.sh
@@ -4,3 +4,4 @@ ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory.ini playbook.yaml
 ansible-playbook -i inventory.ini lbr_setup.yml
 curl -I http://stlb01
 for i in {1..10}; do curl http://stlb01; done
+ 
