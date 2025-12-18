64957061abae0dd00ec14be9815a84158df89b64
Abhijit RAY <royabhijits928@gmail.com>
2025-12-18 22:58:12 +0530

    Create variables.tf

---
+++ Day 98: Launch EC2 in Private VPC Subnet Using Terraform/variables.tf
@@ -0,0 +1,29 @@
+variable "KKE_VPC_CIDR" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "KKE_SUBNET_CIDR" {
+  description = "CIDR block for the subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "AMI ID for EC2 instance"
+  type        = string
+  default     = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI
+}
